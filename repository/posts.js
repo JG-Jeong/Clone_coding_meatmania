fa71870be8594d808afb6ac5533f937a2412d0e3
Seosoohyung <spartaseosu@gmail.com>
2022-10-28 22:26:29 +0900

    2022-10-18 PM10:26 posts 기본기능구현 서수형

--- repository/posts.js
+++ repository/posts.js
@@ -2,8 +2,8 @@ const { Posts } = require("../models");
 
 class PostsRepository {
     
-    createProduct = async ( userId, title, option, amount, cost, item, content, origin, deadline) => {
-        await Posts.create({userId,title,option,amount,cost,item,content,origin,deadline})
+    createProduct = async ( userId, title, option, amount, cost, item, content, origin, deadline,imgUrl) => {
+        await Posts.create({userId,title,option,amount,cost,item,content,origin,deadline,imgUrl})
         return;
     }
     
@@ -15,6 +15,11 @@ class PostsRepository {
     findOneProduct = async(postId) => {
         const findOnePost = await Posts.findOne({where:{postId}});
         return findOnePost;
+    }
+
+    deleteProduct = async(postId) => {
+        await Posts.destroy({where:{postId}});
+        return
     }
     
 }
