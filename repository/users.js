547b1e3f916432a5b7644d435372e6da79fbe2f7
talli0505@naver.com <talli0505@naver.com>
2022-10-28 16:00:51 +0900

    signup login

--- repository/users.js
+++ repository/users.js
@@ -2,27 +2,12 @@ const { Users } = require("../models");
 const { Op } = require("sequelize");
 
 class UsersRepository { 
-  constructor() {
-    this.Users = Users;
-  }
-  
-  // 유저 정보 조회 by 이메일과 닉네임을 위한 함수
-  findUserAccount = async (email, nickname) => {
-
-    // findOne로 email, nickname으로 이루어진 정보가 있는지 확인
-    const findUserAccountData = await this.Users.findOne({
-      where: {
-        [Op.or]: [{ email }, { nickname }],
-      },
-    });
-    return findUserAccountData;
-  };
 
   // 회원가입을 위한 함수
-  createAccount = async (email, nickname, password) => {
+  signUp = async (email, nickname, password) => {
 
     // create로 회원가입
-    const createAccountData = await this.Users.create({
+    const createAccountData = await Users.create({
       email,
       nickname,
       password,
@@ -30,11 +15,23 @@ class UsersRepository {
     return createAccountData;
   };
 
+  // 유저 정보 조회 by 이메일과 닉네임을 위한 함수
+  findUserAccount = async (email, nickname) => {
+
+    // findOne로 email, nickname으로 이루어진 정보가 있는지 확인
+    const findUserAccountData = await Users.findOne({
+      where: {
+        [Op.or]: [{ email }, { nickname }],
+      },
+    });
+    return findUserAccountData;
+  };
+
   // 로그인을 위한 함수
   login = async (email, password) => {
     
     // findOne으로 email, password가 있는지 확인
-    const loginData = await this.Users.findOne({ where: { email, password } });
+    const loginData = await Users.findOne({ where: { email, password } });
     return loginData;
   };
 }
--- router/index.js
+++ router/index.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const postsRouter = require("./posts.router");
-// const usersRouter = require("./users");
+const usersRouter = require("./users.router");
 // const cartsRouter = require("./carts");
 router.use("/posts", postsRouter);
-// router.use("/users", usersRouter);
+router.use("/users", usersRouter);
 // router.use("/carts", cartsRouter);
 
 module.exports = router;
