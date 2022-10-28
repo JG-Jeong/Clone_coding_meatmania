f081e431afd451a9b8722f419ccd4cfb33792857
talli0505@naver.com <talli0505@naver.com>
2022-10-28 11:53:19 +0900

    users model, migration

--- models/users.js
+++ models/users.js
@@ -1,1 +1,30 @@
-//gg
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Users extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Users.init({
+    userId: {
+      primaryKey: true,
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+    },
+    id: DataTypes.STRING,
+    nickname: DataTypes.STRING,
+    password: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'Users',
+  });
+  return Users;
+};
