8fb1e89b8e3314b105162dc0657cc122c877e08a
talli0505@naver.com <talli0505@naver.com>
2022-10-28 12:26:48 +0900

    migration users , models carts 변경

--- models/carts.js
+++ models/carts.js
@@ -19,7 +19,7 @@ module.exports = (sequelize, DataTypes) => {
       allowNull: false,
       autoIncrement: true,
       primaryKey: true,
-      type: Sequelize.INTEGER
+      type: DataTypes.INTEGER
     },
     postId: DataTypes.INTEGER,
     userId: DataTypes.INTEGER,
