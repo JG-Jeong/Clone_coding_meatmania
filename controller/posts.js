de1440da5899b4e59d23e6861eab258a92658990
Seosoohyung <spartaseosu@gmail.com>
2022-10-28 18:58:36 +0900

    2022-10-28 PM06:58 post의 전체조회,상세조회 구현 서수형

--- controller/posts.js
+++ controller/posts.js
@@ -10,6 +10,17 @@ class PostsController {
         await this.postsService.createPost(userId,title,option,amount,cost,item,content,origin,deadline);
         res.send("상품 생성 완료")
     }
+
+    findAllProduct = async (req, res, next) => {
+        const findAllPost = await this.postsService.findAllProduct();
+        res.status(200).json({ data: findAllPost })
+    }
+
+    findOneProduct = async (req, res, next) => {
+        const { postId } = req.params;
+        const findOnePost = await this.postsService.findOneProduct(postId);
+        res.status(200).json({data: findOnePost});
+    }
     
 
 }
