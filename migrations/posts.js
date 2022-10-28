28705fffdb98915b1e6b50360bdd9969d06df996
Seosoohyung <spartaseosu@gmail.com>
2022-10-28 12:24:05 +0900

    post model,migration 생성

--- migrations/posts.js
+++ migrations/posts.js
@@ -1,1 +1,61 @@
-//gg
+"use strict";
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable("Posts", {
+      postId: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        unique: false,
+        allowNull: false,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      option: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      amount: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      cost: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+      },
+      item: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      content: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      origin: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      deadline: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable("Posts");
+  },
+};
