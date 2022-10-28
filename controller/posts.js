c193c9f37c0b589c485a9ea386240b1973f39a42
Seosoohyung <spartaseosu@gmail.com>
2022-10-28 17:48:05 +0900

    posts 1

--- controller/posts.js
+++ controller/posts.js
@@ -4,8 +4,10 @@ class PostsController {
     postsService = new PostsService();
 
     createPost = async(req,res,next) => {
+        const { userId } = res.locals.user;
+        console.log(userId)
         const {title, option, amount, cost, item, content, origin, deadline} = req.body;
-        await this.postsService.createPost(title,option,amount,cost,item,content,origin,deadline);
+        await this.postsService.createPost(userId,title,option,amount,cost,item,content,origin,deadline);
         res.send("상품 생성 완료")
     }
 
