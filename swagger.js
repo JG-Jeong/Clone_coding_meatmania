7f42f3418bb11916b4b111796fea50566c928563
talli0505@naver.com <talli0505@naver.com>
2022-10-31 19:12:11 +0900

    2022-10-31 PM19:11 swagger 수정 조민수

--- swagger.js
+++ swagger.js
@@ -7,6 +7,34 @@ const doc = {
   },
   host: "localhost:3000",
   schemes: ["http"],
+  consumes: ["application/json"],
+  produces: ["application/json"],
+  tags: [
+    {
+      name: "회원 가입API",
+      description: "회원가입이요"
+    },
+    {
+      name: "로그인API",
+      description: ""
+    },
+    {
+      name: "게시물API",
+      description: ""
+    },
+    {
+      name: "장바구니API",
+      description: ""
+    }
+  ],
+  securityDefinitions: {
+    apiKeyAuth: {
+      type: "apiKey",
+      in: "header", // can be "header", "query" or "cookie"
+      name: "Authorization", // name of the header, query parameter or cookie
+      description: "any description...",
+    },
+  },
 };
 
 const outputFile = "./swagger-output.json";
