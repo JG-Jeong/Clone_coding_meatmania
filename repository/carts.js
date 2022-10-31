c4ce00f9f4cbec10baedec6f732e8e2a5034fed8
JG-Jeong <jo128g@naver.com>
2022-11-01 01:49:21 +0900

    2022-11-01 AM01:50 Cart기능구현 소켓x 정정길

--- repository/carts.js
+++ repository/carts.js
@@ -6,22 +6,24 @@ const { badRequestError } = require('../exception/exception')
 class CartsRepository {
 
     //carts에 물건 생성
-    createCarts = async ( postId, userId, option, amount, cost ) => {
+    createCarts = async ( postId, userId, amount ) => {
         //cart에 넣을 상품이 Posts 에 등록된 제품이 아닌경우 오류 반환
         try{
+            //body로 받아온 postId로 Post에서 어떤 제품인지 찾는다.
             const findPosts = await Posts.findOne({
                 where : {
-                    postId,
+                    postId
                 },})
+            console.log(findPosts)
             if(!findPosts) {
-                throw new badRequestError
+                throw new badRequestError; // 제품이 Post에 없는 제품이면 404 베드 리퀘스트 를 띄운다.
             }
             //카트에 이미 추가된 상품이면,
             const findCarts = await Carts.findOne({
                 where : {
                     postId,
                     userId,
-                    option
+                    amount
                 },})
             // 이미 추가된 상품에 amount를 더해준다.
             if (findCarts) {
@@ -30,15 +32,16 @@ class CartsRepository {
                     where : {
                         postId,
                         userId,
-                        option
                     },})
             }else {
+                const option = findPosts.option;
+                const cost = findPosts.cost;
                 const carts = await Carts.create({ postId, userId, option, amount, cost })
                 return carts;
             }
         }catch (error){
             console.error(error);
-            return (error)
+            throw (error)
         }
     };
 
@@ -66,7 +69,6 @@ class CartsRepository {
             {where: { postId, userId }})
         return indiCarts
     }
-    
 }
 
 module.exports = CartsRepository
